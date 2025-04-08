1400712d9ceb71eb286d55872304ec0af1f1d453
Evolu-Web <contact@evolu-web.fr>
2025-04-08 10:30:24 +0200

    Add models/Carousel.js 1744101024504

---
+++ models/Carousel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const CarouselSchema = new mongoose.Schema({
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Carousel', CarouselSchema);
